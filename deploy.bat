663e87b9d0265904512d49329b05d3c560b45329
Hursev <51803976+Hursev@users.noreply.github.com>
2026-02-08 14:47:25 -0800

    Fix description and add deploy.baat

---
+++ deploy.bat
@@ -0,0 +1,2 @@
+rem robocopy "." "C:\Games\World of Warcraft-USA\_retail_\Interface\AddOns\!MyAddon" /E /XD .git .vs /XF deploy.bat *.sln .git*
+robocopy . "C:\temp\a" /E /XD .git .vs /XF deploy.bat *.sln .git* /XA:H /R:0 /W:0
